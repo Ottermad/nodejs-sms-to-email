df77cbe000302ac90a6a6ad7dccd4b8a3017b616
Ottermad <charlie.thomas@attwoodthomas.net>
2015-01-03 23:34:05 +0000

    Init

---
+++ main.js
@@ -0,0 +1,27 @@
+var bodyParser = require('body-parser')
+var express = require('express');
+var app     = express();
+var sendgrid  = require('sendgrid')('Ottermad','OttersR0ck');
+
+app.use(bodyParser());
+
+app.post('/', function(req, res) {
+ 	console.log(req.body);
+	var message_array = req.body.Body.split(",");
+	console.log(message_array);
+	
+	sendgrid.send({
+	    to:       message_array[0],
+	    from:     message_array[1],
+	    subject:  message_array[2],
+	    text:     message_array[3]
+	  }, function(err, json) {
+	    if (err) { return console.error(err); }
+	    console.log(json);
+	});
+  });
+    
+app.listen(8080, function() {
+  console.log('Server running at http://127.0.0.1:8080/');
+});
+
